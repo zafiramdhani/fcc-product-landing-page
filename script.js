6237936d1813ab2967ccecff91664112cff73d93
zafiramdhani <izzafiramdhanialkaff@gmail.com>
2022-01-26 11:41:02 +0700

    change nav scroll amount and iframe height

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const hdr = document.querySelector('header');
 window.onscroll = function() {
   var top = window.scrollY;
-  if (top >= 150) {
+  if (top >= 50) {
     hdr.classList.add('active');
   } else {
     hdr.classList.remove('active');
